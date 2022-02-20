643fac628a6cc43453d11c92c9c312a059602400
mjenks <rikku.hotaru@gmail.com>
2022-02-20 15:57:28 -0500

    part 1
    part 1 solved

--- day1.py
+++ day1.py
@@ -20,9 +20,34 @@ def parse(puzzle_input):
 
 #solve
 def solve(puzzle_data):
-
+    facing = 0 #compass North: 0 East:1 South:2 West: 3 
+    position = (0,0)
+    visited = []
+    
+    for direction in puzzle_data:
+        x, y = position
+        turn, distance = direction
         
-    return 0
+        #left turn is the compass value +3 mod 4 (essentially -1 but avoids negative values)
+        if turn == 'L':
+            facing = (facing + 3) % 4
+        #right turn in compass value +1 mod 4 (mod moves from west back to north)
+        elif turn == 'R':
+            facing = (facing + 1) % 4
+        
+        #Now move position based on the new facing
+        if facing == 0:
+            position = x, y + distance
+        elif facing == 1:
+            position = x + distance, y
+        elif facing == 2:
+            position = x, y - distance
+        elif facing == 3:
+            position = x - distance, y
+            
+        visited.append(position)
+            
+    return abs(position[0]) + abs(position[1]), visited
 
 #run and print solution 
 puzzle_path = "input_day1.txt"
