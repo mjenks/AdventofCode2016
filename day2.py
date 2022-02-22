8f0aeed8678df1736a9b9c9fa84a93dec8a1eb3c
mjenks <rikku.hotaru@gmail.com>
2022-02-21 21:44:17 -0500

    solved
    solution code to solve both parts of day 2

--- day2.py
+++ day2.py
@@ -24,11 +24,43 @@ def find_button(key, code):
             key = (key[0], key[1] + 1)
     
     return key
+    
+def find_button2(key, code):
+    for inst in code:
+        if inst == 'U' and key[0] > 1:
+            if key[1] <= 3 and sum(key) > 4:
+                key = (key[0] - 1, key[1])
+            elif key[1] > 3:
+                if key != (2,4) and key != (3,5):
+                    key = (key[0] - 1, key[1])
+        elif inst == 'D' and key[0] < 5:
+            if key[1] >= 3 and sum(key) < 8:
+                key = (key[0] + 1, key[1])
+            elif key[1] < 3:
+                if key != (3, 1) and key != (4,2):
+                    key = (key[0] + 1, key[1])
+        elif inst == 'L' and key[1] > 1:
+            if key[0] <= 3 and sum(key) > 4:
+                key = (key[0], key[1] -1)
+            elif key[0] > 3:
+                if key != (4,2) and key != (5,3):
+                   key = (key[0], key[1] -1) 
+        elif inst == 'R' and key[1] < 5:
+            if key[0] >= 3 and sum(key) < 8:
+                key = (key[0], key[1] + 1)
+            elif key[0] < 3:
+                if key != (1, 3) and key != (2,4):
+                    key = (key[0], key[1] + 1)
+    
+    return key
 
 def solve(puzzle_data):
     key = (2, 2) #define key locations by row column
+    key2 = (3, 1)
     buttons = []
+    buttons2 = []
     code = []
+    code2 = []
     
     for line in puzzle_data:
         key = find_button(key, line)
@@ -57,8 +89,46 @@ def solve(puzzle_data):
                 code.append(8)
             elif button[1] == 3:
                 code.append(9)
+                
+    for line in puzzle_data:
+        key2 = find_button2(key2, line)
+        buttons2.append(key2)
+        
+    #convert the button location into number values
+    for button in buttons2:
+        if button[0] == 1:
+            if button[1] == 3:
+                code2.append(1)
+        elif button[0] == 2:
+            if button[1] == 2:
+                code2.append(2)
+            elif button[1] == 3:
+                code2.append(3)
+            elif button[1] == 4:
+                code2.append(4)
+        elif button[0] == 3:
+            if button[1] == 1:
+                code2.append(5)
+            elif button[1] == 2:
+                code2.append(6)
+            elif button[1] == 3:
+                code2.append(7)
+            elif button[1] == 4:
+                code2.append(8)
+            elif button[1] == 5:
+                code2.append(9)
+        elif button[0] == 4:
+            if button[1] == 2:
+                code2.append('A')
+            elif button[1] == 3:
+                code2.append('B')
+            elif button[1] == 4:
+                code2.append('C')
+        elif button[0] == 5:
+            if button[1] == 3:
+                code2.append('D')
     
-    return ''.join(str(code)), 0
+    return code, code2
 
 puzzle_path = "input_day2.txt"
 with open(puzzle_path) as f:
