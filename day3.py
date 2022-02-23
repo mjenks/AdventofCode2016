c59b7467e6df7cb47d2530db6241408e41543659
mjenks <rikku.hotaru@gmail.com>
2022-02-23 11:47:14 -0500

    part 2
    reparsed the data as described in part 2 of the puzzle. all parts solved

--- day3.py
+++ day3.py
@@ -16,6 +16,35 @@ def parse(puzzle_input):
     
     return data
     
+def parse2(puzzle_input):
+    data = []
+    i = 1
+    for line in puzzle_input:
+        line = line.strip().split()
+        if i == 1:
+            tri1 = [int(line[0])]
+            tri2 = [int(line[1])]
+            tri3 = [int(line[2])]
+        if i == 2:
+            tri1.append(int(line[0]))
+            tri2.append(int(line[1]))
+            tri3.append(int(line[2]))
+        if i == 3:
+            tri1.append(int(line[0]))
+            tri2.append(int(line[1]))
+            tri3.append(int(line[2]))
+            tri1.sort()
+            tri2.sort()
+            tri3.sort()
+            data.append(tri1)
+            data.append(tri2)
+            data.append(tri3)
+            i = 0
+        i+=1
+            
+            
+    return data
+    
 def solve(puzzle_data):
     possible = 0
     
@@ -23,15 +52,17 @@ def solve(puzzle_data):
         if triangle[0] + triangle[1] > triangle[2]:
             possible += 1
     
-    return possible, 0
+    return possible
 
 puzzle_path = "input_day3.txt"
 with open(puzzle_path) as f:
     puzzle_input = f.readlines()
     
 puzzle_data = parse(puzzle_input)
+puzzle_data2 = parse2(puzzle_input)
 
-solution1, solution2 = solve(puzzle_data)
+solution1 = solve(puzzle_data)
+solution2 = solve(puzzle_data2)
 
 print(solution1)
 print(solution2)
