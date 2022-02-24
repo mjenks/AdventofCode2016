95ec88387d372c3a00a56592aa8555f0a5220ff2
mjenks <rikku.hotaru@gmail.com>
2022-02-24 11:31:03 -0500

    part 1
    code now solves part 1 of the puzzle

--- day4.py
+++ day4.py
@@ -5,6 +5,8 @@ Created on Thu Feb 24 10:41:41 2022
 @author: mjenks
 """
 
+import collections
+
 def parse(puzzle_input):
     data = []
     for room in puzzle_input:
@@ -17,8 +19,44 @@ def parse(puzzle_input):
         data.append((name, sector_id, checksum))
     return data
    
+def isReal(room):
+    name, sector, check = room
+    name = ''.join(name)
+    letter_count = collections.Counter(name)
+    c = 0
+    samecount = []
+    checksum = []
+    for letter, count in letter_count.most_common(26):
+        if count < c:
+            same = sorted(samecount)
+            for l in same:
+                checksum.append(l)
+            samecount = []
+        
+        samecount.append(letter)
+        c = count
+    same = sorted(samecount)
+    for l in same:
+        checksum.append(l)
+    checksum = ''.join(checksum)   
+       
+    if check == checksum[:5]:
+        return True
+    else:
+        return False
+    
+   
 def solve(puzzle_data):
-    return 0,0
+    sector_sum = 0
+    valid_rooms = 0
+    
+    for room in puzzle_data:
+        if isReal(room):
+            sector_sum += int(room[1])
+            valid_rooms += 1
+    
+
+    return sector_sum,0
 
 puzzle_path = "input_day4.txt"
 with open(puzzle_path) as f:
