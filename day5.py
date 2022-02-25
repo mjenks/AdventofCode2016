907777973b47f2f444e32623d1cdb7d2219f391c
mjenks <rikku.hotaru@gmail.com>
2022-02-25 12:39:22 -0500

    day 5 start
    Code outline and puzzle input

---
+++ day5.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 25 12:35:05 2022
+
+@author: mjenks
+"""
+
+
+def solve(puzzle_data):
+    
+    return 0, 0
+
+
+    
+puzzle_data = "ffykfhsq"
+solution1, solution2 = solve(puzzle_data)
+
+print(solution1)
+print(solution2)
