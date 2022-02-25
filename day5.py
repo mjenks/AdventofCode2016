0d3ec987c69df0b40483d6d7f515d0c80768ec11
mjenks <rikku.hotaru@gmail.com>
2022-02-25 13:12:35 -0500

    day 5 solved
    day 5 part 1 and 2 solved. Includes a print line to display the slowly
    discoved door code for part 2.

--- day5.py
+++ day5.py
@@ -5,15 +5,31 @@ Created on Fri Feb 25 12:35:05 2022
 @author: mjenks
 """
 
+import hashlib
 
 def solve(puzzle_data):
+    code = []
+    code_2 = ['_', '_', '_', '_', '_', '_', '_', '_']
+    i = 0
+    while '_' in code_2:
+        hash_val = hashlib.md5(puzzle_data.encode('utf-8') + str(i).encode('utf-8')).hexdigest()
+        if hash_val.startswith('00000'):
+            if len(code) < 8:
+                code.append(hash_val[5])
+            j = int(hash_val[5], 16)
+            if j < 8 and code_2[j] is '_':
+                code_2[j] = hash_val[6]
+                print ''.join(code_2)
+                
+                
+        i += 1
+            
     
-    return 0, 0
+    return ''.join(code), ''.join(code_2)
 
 
     
 puzzle_data = "ffykfhsq"
 solution1, solution2 = solve(puzzle_data)
 
-print(solution1)
-print(solution2)
+print solution1, solution2
