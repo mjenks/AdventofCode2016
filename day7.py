54323fe25cca692b720f7a8081934b6474015c13
mjenks <rikku.hotaru@gmail.com>
2022-02-27 11:06:36 -0500

    part 2
    part 2 solved

--- day7.py
+++ day7.py
@@ -46,15 +46,44 @@ def tls_supported(ip):
             
     return supported
 
-        
-    
+def ssl_supported(ip):
+    supported = False
+    supernet = []
+    hypernet = []
+    for part in ip.split('['):
+        if ']' in part:
+            hypernet.append(part.split(']')[0])
+            supernet.append(part.split(']')[1])
+        else:
+            supernet.append(part)
+    aba = []
+    for part in supernet:
+        seq = list(part)
+        l1 = seq[0]
+        l2 = seq[1]
+        for letter in seq[2:]:
+            l3 = letter
+            if l1 == l3 and l2 != l1:
+                aba.append(l1+l2+l3)
+            l1 = l2
+            l2 = l3
+    hypernet = ' '.join(hypernet)
+    for seq in aba:
+        bab = seq[1] + seq[0] + seq[1]
+        if bab in hypernet:
+            supported = True
+                
+    return supported
     
 def solve(puzzle_input):
     tls_count = 0
+    ssl_count = 0
     for ip in puzzle_input:
         if tls_supported(ip):
             tls_count += 1
-    return tls_count,0
+        if ssl_supported(ip):
+            ssl_count += 1
+    return tls_count, ssl_count
 
 puzzle_path = "input_day7.txt"
 with open(puzzle_path) as f:
