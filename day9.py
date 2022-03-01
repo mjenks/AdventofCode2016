9d767bdae099a251d3a4ae28d3aab923e60d1616
mjenks <rikku.hotaru@gmail.com>
2022-03-01 14:09:51 -0500

    part 2
    it takes far to long to try to decompress the string completely for part
    2 so instead I use a weight counting method to simply find the length of
    the fully decompressed input and not what it would actually be.

--- day9.py
+++ day9.py
@@ -9,28 +9,53 @@ def parse(puzzle_input):
     data = puzzle_input.strip()
     
     return data
-
-def solve(puzzle_data):
+    
+def decompress(string):
     i = 0
     decompressed = ''
-    while i < len(puzzle_data):
-        char = puzzle_data[i]
+    while i < len(string):
+        char = string[i]
         if char == '(':
             i += 1
-            marker = puzzle_data[i:].split(')')[0]
+            marker = string[i:].split(')')[0]
             vals = marker.split('x')
             a = int(vals[0]) #num of chars
             b = int(vals[1]) #times repeated
             marker_len = len(marker) + 1
             i += marker_len
             for n in range(b):
-                decompressed += puzzle_data[i:i+a]
+                decompressed += string[i:i+a]
             i += a
         else:
             decompressed += char
             i += 1
+    return decompressed
+
+def solve(puzzle_data):
+    first = decompress(puzzle_data)
+    weight = [1 for x in puzzle_data]
+    index = 0
+    length = 0
+    while index < len(puzzle_data):
+        char = puzzle_data[index]
+        if char == '(':
+            index +=1
+            marker = puzzle_data[index:].split(')')[0]
+            vals = marker.split('x')
+            a = int(vals[0]) #num of chars
+            b = int(vals[1]) #multiplier
+            marker_len = len(marker) + 1
+            index += marker_len
+            for n in range(a):
+                if n+index < len(weight):
+                    weight[n+index] = b*weight[n+index]
+        else:
+            length += weight[index]
+            index += 1
+        
     
-    return len(decompressed),0
+    
+    return len(first), length
     
 puzzle_path = "input_day9.txt"
 with open(puzzle_path) as f:
