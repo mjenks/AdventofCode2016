8cc2cf6601365d5e31e24656f132b32b1c272964
mjenks <rikku.hotaru@gmail.com>
2022-03-02 11:37:48 -0500

    part 1
    give bots the assigned chips then act until answer is found

--- day10.py
+++ day10.py
@@ -23,7 +23,33 @@ def parse(puzzle_input):
     return data
     
 def solve(puzzle_data):
-    return 0,0
+    start, actions = puzzle_data
+    #make a list of bots with a list of chips each has
+    bots = [[] for i in range(len(actions))]
+    unfound = True
+    wanted = ''
+
+    #set up by giving each bot chips it is assigned
+    for inst in start:
+        chip, bot = inst
+        bots[bot].append(chip)
+        
+    #have bots act until a bot has chip 61 and 17
+    while unfound:
+        for i in range(len(bots)):
+            if len(bots[i]) == 2:
+                held = bots[i]
+                name = 'bot ' + str(i)
+                if 61 in held and 17 in held:
+                    wanted = name
+                    unfound = False
+                held.sort()
+                low, high = actions[name]
+                bots[low].append(held[0])
+                bots[high].append(held[1])
+                bots[i] = []
+
+    return wanted,0
 
 puzzle_path = "input_day10.txt"
 with open(puzzle_path) as f:
