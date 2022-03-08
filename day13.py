dee3b7e36ec9ec3649507f88bbf86019308fcfd1
mjenks <rikku.hotaru@gmail.com>
2022-03-08 11:54:49 -0500

    part 1
    part 1 solved: maps all the locations that can be reached in steps and
    stops once the destination in reachable and returns that

--- day13.py
+++ day13.py
@@ -5,19 +5,56 @@ Created on Mon Mar 07 10:18:16 2022
 @author: mjenks
 """
 
-def parse(puzzle_input):
-    data = []
-    return data
+def isopen(x, y):
+    if x < 0 or y < 0:
+        return False
+    global puzzle_input
+    number = x*x + 3*x + 2*x*y + y + y*y + puzzle_input
+    binary = [] #hold the 1's and 0's of the binary in reverse order
+    while number != 0:
+        binary.append(number%2)
+        number = number//2
+    count = sum(binary)
+    if count%2 == 0:
+        return True
+    else:
+        return False
+        
+def move(current):
+    options = []
+    for point in current:
+        x, y = point
+        if isopen(x+1,y) and (x+1,y) not in visited:
+            spot = x+1, y
+            visited.add(spot)
+            options.append(spot)
+        if isopen(x,y+1) and (x,y+1) not in visited:
+            spot = x, y+1
+            visited.add(spot)
+            options.append(spot)
+        if isopen(x-1, y) and (x-1,y) not in visited:
+            spot = x-1, y
+            visited.add(spot)
+            options.append(spot)
+        if isopen(x, y-1) and (x,y-1) not in visited:
+            spot = x, y-1
+            visited.add(spot)
+            options.append(spot)
+    return options
     
-def solve(puzzle_data):
-    return 0, 0
+def solve():
+    location = [(1,1)]
+    steps = 0
+    while (31,39) not in visited:
+        steps += 1
+        location = move(location)
+    
+    return steps, 0
 
-puzzle_path = "input_day13.txt"
-with open(puzzle_path) as f:
-    puzzle_input = f.readlines()
-    
-puzzle_data = parse(puzzle_input)
-solution1, solution2 = solve(puzzle_data)
+visited = set()
+puzzle_input = 1364
+
+solution1, solution2 = solve()
 
 print(solution1)
 print(solution2)
