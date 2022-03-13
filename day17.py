be04b21c948154627f76fad51786775ea838c105
mjenks <rikku.hotaru@gmail.com>
2022-03-13 17:11:35 -0400

    day 17
    set up code

---
+++ day17.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 13 17:04:27 2022
+
+@author: mjenks
+"""
+
+import hashlib
+    
+def solve(puzzle_data):
+    return 0, 0
+
+
+    
+puzzle_data = 'njfxhljp'
+solution1, solution2 = solve(puzzle_data)
+
+print(solution1)
+print(solution2)
