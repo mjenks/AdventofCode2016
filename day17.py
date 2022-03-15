37c3345fa59cc871296cc5ae32ceae6c390c8d29
mjenks <rikku.hotaru@gmail.com>
2022-03-15 13:36:35 -0400

    check doors
    added a function to show find which doors are open

--- day17.py
+++ day17.py
@@ -6,8 +6,30 @@ Created on Sun Mar 13 17:04:27 2022
 """
 
 import hashlib
+
+def opendoors(passcode, path):
+    hashcode = hashlib.md5(passcode + ''.join(path)).hexdigest()
+    good = {'b', 'c', 'd', 'e', 'f'}
+    options = []
+    #up
+    if hashcode[0] in good:
+        options.append('U')
+    #down
+    if hashcode[1] in good:
+        options.append('D')
+    #left
+    if hashcode[2] in good:
+        options.append('L')
+    #right
+    if hashcode[3] in good:
+        options.append('R')
+    return options
+        
     
 def solve(puzzle_data):
+    path = []
+    location = (1,1)
+    
     return 0, 0
 
 
