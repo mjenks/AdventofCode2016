2fe6b3f2471b25d0c4116fea4732d8f880fa4181
mjenks <rikku.hotaru@gmail.com>
2022-03-15 15:23:24 -0400

    part 1
    part 1 solved

--- day17.py
+++ day17.py
@@ -6,36 +6,62 @@ Created on Sun Mar 13 17:04:27 2022
 """
 
 import hashlib
+passcode = 'njfxhljp'
 
-def opendoors(passcode, path):
+def opendoors(path, location):
+    global passcode
     hashcode = hashlib.md5(passcode + ''.join(path)).hexdigest()
     good = {'b', 'c', 'd', 'e', 'f'}
     options = []
     #up
-    if hashcode[0] in good:
+    if hashcode[0] in good and location[1] != 1:
         options.append('U')
     #down
-    if hashcode[1] in good:
+    if hashcode[1] in good and location[1] != 4:
         options.append('D')
     #left
-    if hashcode[2] in good:
+    if hashcode[2] in good and location[0] != 1:
         options.append('L')
     #right
-    if hashcode[3] in good:
+    if hashcode[3] in good and location[0] != 4:
         options.append('R')
     return options
-        
+         
     
-def solve(puzzle_data):
-    path = []
-    location = (1,1)
-    
-    return 0, 0
+def solve():
+    #holds the path taken and current location of all options on each step
+    current = [([], (1,1))]
+    vault_reached = False
+    steps = 0
+    while not vault_reached:
+        step = [] #hold all possible steps
+        for path, location in current:
+            if location == (4,4):
+                vault_reached = True
+                short_path = path
+            options = opendoors(path, location)
+            for direction in options:
+                new = path[:]
+                new.append(direction)
+                x, y = location
+                if direction == 'U':
+                    y -= 1
+                elif direction == 'D':
+                    y += 1
+                elif direction == 'L':
+                    x -= 1
+                elif direction == 'R':
+                    x += 1
+                step.append((new, (x,y)))
+                
+        current = step[:]
+        steps += 1
+                
+    return ''.join(short_path), 0
 
 
     
-puzzle_data = 'njfxhljp'
-solution1, solution2 = solve(puzzle_data)
+solution1, solution2 = solve()
 
 print(solution1)
 print(solution2)
