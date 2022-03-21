d99797d97ea2b3adfba0a043e035d27515e983d7
mjenks <rikku.hotaru@gmail.com>
2022-03-21 15:08:21 -0400

    part 1
    figured out how to find this mathematically

--- day19.py
+++ day19.py
@@ -4,9 +4,23 @@ Created on Mon Mar 21 14:07:25 2022
 
 @author: mjenks
 """
+import math
+
+def find_power(num):
+    power = 0
+    while num//2 != 0:
+        power += 1
+        num = num//2
+    return power
     
 def solve(puzzle_data):
-    return 0, 0
+    #part one is odd number that would be refrenced by the index (in a list of odds) of the remaining value after removing the nearest power of 2
+    #find the nearest power of 2
+    power = find_power(puzzle_data)
+    remaining = puzzle_data - math.pow(2, power)
+            
+
+    return remaining*2 + 1, 0
 
     
 puzzle_data = 3014387
