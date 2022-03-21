5f1239c553dac2e4ac66757d867690dd9b516470
mjenks <rikku.hotaru@gmail.com>
2022-03-21 14:10:56 -0400

    day 19
    set up solution code with puzzle data set from my puzzle input

---
+++ day19.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 14:07:25 2022
+
+@author: mjenks
+"""
+    
+def solve(puzzle_data):
+    return 0, 0
+
+    
+puzzle_data = 3014387
+solution1, solution2 = solve(puzzle_data)
+
+print(solution1)
+print(solution2)
