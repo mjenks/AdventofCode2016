b2c8a2ade4b85e7b9cf4a976e0120debc0df1f17
mjenks <rikku.hotaru@gmail.com>
2022-03-22 15:36:19 -0400

    testing to find pattern
    wrote code to do work out the last elf for a given number. This would
    take to long to solve with my input, so I use it to for 1 to 100 elves
    to find a pattern

--- day19.py
+++ day19.py
@@ -6,6 +6,21 @@ Created on Mon Mar 21 14:07:25 2022
 """
 import math
 
+def findElf(num):
+    elves = range(1,num+1)
+    i = 0
+    while len(elves) > 1:
+        num = len(elves)
+        elf = elves[i]
+        out = elves[(i + num//2)%num]
+        elves.remove(elves[(i + num//2)%num])
+        if out > elf:
+            i = (i+1)%len(elves)
+        else:
+            i = i%len(elves)
+        
+    return elves[0]
+
 def find_power(num):
     power = 0
     while num//2 != 0:
@@ -19,12 +34,16 @@ def solve(puzzle_data):
     power = find_power(puzzle_data)
     remaining = puzzle_data - math.pow(2, power)
             
+    first100 = []
+    for i in range(1,101):
+        first100.append(findElf(i))
 
-    return remaining*2 + 1, 0
+    return remaining*2 + 1, first100
 
     
 puzzle_data = 3014387
 solution1, solution2 = solve(puzzle_data)
+
 
 print(solution1)
 print(solution2)
